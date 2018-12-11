ea022a8eeefb10f24ae98c2b940293cf616746e4
Julia Ballas <42751526+jballas@users.noreply.github.com>
2018-12-10 18:34:18 -0700

    Update sketch.js
    I created the eyes, but I can't get them to blink. Plus, I need to manipulate the size and shapes of the eyes. I don't want all ovals.

--- blinking-eyes/sketch.js
+++ blinking-eyes/sketch.js
@@ -1,12 +1,43 @@
+let x = 50;
+let y = 50;
+let width = 100;
+let height = 50;
+let eye_x;
+let eye_y;
+let lid;
+let blink = 1;
+
+
 function setup() {
   createCanvas(300, 300)
 }
 
 function draw() {
   background(0);
-
   fill(255);
-  ellipse(50, 50, 100, 50);
-  ellipse(100, 100, 50, 20);
 
+  displayEye(x, y, width, height);
+  displayEye(150, 150, width, height);
+
+}
+
+function displayEye(x,y, w,h){
+  eye_x = x;
+  eye_y = y;
+  lid = eye_y - h;
+  // Eye
+  ellipse(eye_x, eye_y, w, h);
+
+  // // Eyelid
+  // push();
+  //   rectMode(CENTER);
+  //   fill(255);
+  //   rect(x, lid, w, h);
+  // pop();
+
+  // Pupil
+  push();
+    strokeWeight(5);
+    line(x,y, x, y+w);
+  pop();
 }
