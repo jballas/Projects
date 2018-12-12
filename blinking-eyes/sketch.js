e55765d4f4af5b8682e69c0349f007f8576d15f1
Julia Ballas <42751526+jballas@users.noreply.github.com>
2018-12-11 20:45:40 -0700

    Update sketch.js
    the eye still doesn't really blink. I made an eyelid that drops down, but that's not really a blink. I probably need different math for this one.

--- blinking-eyes/sketch.js
+++ blinking-eyes/sketch.js
@@ -1,12 +1,10 @@
-let x = 50;
+let x = 150;
 let y = 50;
 let width = 100;
 let height = 50;
-let eye_x;
-let eye_y;
-let lid;
-let blink = 1;
-
+let lid_x = 150;
+let lid_y = 50;
+blink = 1;
 
 function setup() {
   createCanvas(300, 300)
@@ -16,28 +14,40 @@ function draw() {
   background(0);
   fill(255);
 
+
   displayEye(x, y, width, height);
   displayEye(150, 150, width, height);
 
+// Eyelid
+  push();
+    fill(0);
+    rectMode(CENTER);
+    rect(lid_x, lid_y-50, width, height)
+  pop();
+
+lid_y += blink;
+
+  if (lid_y >= 100){
+       blink *= -1;
+    }
+  if (lid_y <= 50){
+    blink*= -1;
+  }
+
+//console.log(lid_y);
+
 }
 
-function displayEye(x,y, w,h){
-  eye_x = x;
-  eye_y = y;
-  lid = eye_y - h;
-  // Eye
-  ellipse(eye_x, eye_y, w, h);
+function displayEye(x, y, w, h){
 
-  // // Eyelid
-  // push();
-  //   rectMode(CENTER);
-  //   fill(255);
-  //   rect(x, lid, w, h);
-  // pop();
+  // Eye
+  ellipse(x, y, w, h);
 
   // Pupil
   push();
     strokeWeight(5);
     line(x,y, x, y+w);
   pop();
+
+
 }
