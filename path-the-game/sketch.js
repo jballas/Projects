5e063a0254d80b7db54a1e7f280b1b338d17ea17
Julia Ballas <42751526+jballas@users.noreply.github.com>
2019-08-03 09:22:54 -0600

    Update sketch.js
    added path, player and started to work on the player movement.
    What if you can only move in one direction on the path?

--- path-the-game/sketch.js
+++ path-the-game/sketch.js
@@ -1,7 +1,41 @@
+
 function setup() {
-  // put setup code here
+  createCanvas( 500, 500);
 }
 
 function draw() {
-  // put drawing code here
+  background(200);
+  posX = width/2;
+  posY = width/2;
+
+  //print(mouseX, mouseY);
+
+  // path //
+  strokeWeight(5);
+  stroke(0);
+  line(width/2-20, 0, width/2-20, height);
+  line(width/2+20, 0, width/2+20, height);
+
+  // player //
+  push();
+  noStroke();
+    playerDisplay(posX,posY);
+  pop();
+}
+
+function playerDisplay(x,y){
+
+  fill('rgb(0,0,255)');
+  rectMode(CENTER);
+  rect(x, y, 15, 15);
+
+}
+
+function keyPressed(){
+
+    if (keyCode === UP_ARROW){
+      xPos += 1
+    }
+    return false;
+
 }
